c2b2bfe5ebf6c742435492cdf64bb8f3122611a4
lmno3418 <lmno3418@gmail.com>
2025-01-12 19:19:52 +0530

    2nd day

---
+++ day2.a.py
@@ -0,0 +1,7 @@
+#Write a program to take name, age, and gender as input, then print an introduction.
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+gender=input("Enter your gender: ")
+
+print(f"Hello my name is {name}, I am a {gender} and my age is {age}.")
