05a854a7baa8bf036955d611d36f0deb7d2c7943
lmno3418 <lmno3418@gmail.com>
2025-01-13 21:07:42 +0530

    Better code, 3rd day loop

--- day3.b.py
+++ day3.b.py
@@ -1,7 +1,6 @@
 # Find the sum of digits of a number using a loop.
 n = input("Enter a number: ")
-l = len(n)
-sum = 0
-for i in range(0, l, 1):
-    sum += int(n[i])
+sum=0
+for i in range(len(n)):
+    sum+=int(n[i])
 print(sum)
