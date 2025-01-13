5b2f88460d5900c8f1414a71cdb172a8264f44c5
lmno3418 <lmno3418@gmail.com>
2025-01-13 22:22:19 +0530

    Pattern not understood

---
+++ day3.c.py
@@ -0,0 +1,14 @@
+#Create a pattern such as Floyd’s triangle.
+"""
+1
+2 3
+4 5 6
+7 8 9 10
+"""
+r=int(input("Enter Rows: "))
+for i in range(1,+r):
+    for j in range(1,+i):
+        print(i+j,end=" ")
+    print()
+    
+#Learn Patterns Ashish, you are weak here !!!!
