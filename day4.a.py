dc15ffe0d50ead8749dcde434890f8a5b4461548
lmno3418 <lmno3418@gmail.com>
2025-01-14 19:38:02 +0530

    day 4 contions

---
+++ day4.a.py
@@ -0,0 +1,6 @@
+#Check if a number is divisible by both 3 and 5.
+n=int(input("Enter any no to check divisibility from 3 and 5: "))
+if n%3==0 and n%5==0:
+    print("Yes Div..")
+else:
+    print("Not Div..")
